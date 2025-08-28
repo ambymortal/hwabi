076dbb4c4102e6e3e48f66d7273e6d51932cf136
Ambrose Frost <thefirstfrost@proton.me>
2025-08-27 21:18:43 -0400

    clientsession Refactored current clientsession code out of main. Also, added a loop in handleConnection() that keeps the client open attempting to read instead of dropping connection.

---
+++ clientsession.go
@@ -0,0 +1,75 @@
+package main
+
+import (
+	"crypto/rand"
+	"encoding/binary"
+	"encoding/hex"
+	"log"
+	"net"
+)
+
+type ClientSession struct {
+	conn net.Conn
+	send []byte
+	recv []byte
+}
+
+func handleConnection(conn net.Conn) {
+	defer conn.Close()
+
+	addr := conn.RemoteAddr().String()
+	log.Printf("new client connected from: %s", addr)
+
+	session := &ClientSession{
+		conn: conn,
+		send: make([]byte, 4),
+		recv: make([]byte, 4),
+	}
+
+	rand.Read(session.send)
+	rand.Read(session.recv)
+
+	pkt, err := onHandshake(session.recv, session.send)
+	if err != nil {
+		log.Printf("failed to build handshake: %v", err)
+		return
+	}
+
+	// send handshake to client
+	_, err2 := session.conn.Write(pkt)
+	if err2 != nil {
+		log.Printf("handshake error %s: %v", addr, err2)
+		return
+	}
+	log.Printf("sent handshake to %s", addr)
+
+	buffer := make([]byte, 1024)
+
+	// keep connection open
+	for {
+		n, err := session.conn.Read(buffer)
+		if err != nil {
+			log.Printf("disconnected from %s read error: %v", addr, err)
+			return
+		}
+
+		data := buffer[:n]
+
+		// handle crypto next
+
+		log.Printf("received %d bytes from %s: %s",
+			n, addr, hex.EncodeToString(data))
+	}
+}
+
+func onHandshake(recv, send []byte) ([]byte, error) {
+	pkt := NewPacket()
+
+	pkt.WriteShort(95)                             // version
+	pkt.WriteString("1")                           // patch
+	pkt.WriteInt(binary.LittleEndian.Uint32(recv)) // Recv
+	pkt.WriteInt(binary.LittleEndian.Uint32(send)) // Send
+	pkt.WriteByte(8)                               // locale
+
+	return pkt.ToBytes()
+}
--- main.go
+++ main.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"encoding/hex"
 	"fmt"
 	"log"
 	"net"
@@ -25,48 +24,4 @@ func main() {
 
 		go handleConnection(conn)
 	}
-}
-
-func handleConnection(conn net.Conn) {
-	defer conn.Close()
-
-	addr := conn.RemoteAddr().String()
-	log.Printf("new client connected from: %s", addr)
-
-	pkt, err := onHandshake()
-	if err != nil {
-		log.Printf("failed to build handshake: %v", err)
-		return
-	}
-
-	// send handshake to client
-	_, err2 := conn.Write(pkt)
-	if err2 != nil {
-		log.Printf("handshake error %s: %v", addr, err2)
-		return
-	}
-	log.Printf("sent handshake to %s", addr)
-
-	buffer := make([]byte, 1024)
-
-	//attempt to read what the client is sending back
-	n, err := conn.Read(buffer)
-	if err != nil {
-		log.Printf("disconnected from %s read error: %v", addr, err)
-		return
-	}
-
-	log.Printf("received %d bytes from %s: %s", n, addr, hex.EncodeToString(buffer[:n]))
-}
-
-func onHandshake() ([]byte, error) {
-	pkt := NewPacket()
-
-	pkt.WriteShort(95)       // version
-	pkt.WriteString("1")     // patch
-	pkt.WriteInt(0x11223344) // Recv
-	pkt.WriteInt(0xAABBCCDD) // Send
-	pkt.WriteByte(8)         // locale
-
-	return pkt.ToBytes()
 }
